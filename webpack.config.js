1b65a7f9d6fbca6c0f8dd21ed0eb8acf92ebdeaf
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-08-27 21:46:46 +0530

    Add html-loader to handle images in the html files

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,40 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "bundle.js",
-		clean: true,
-	},
-	devtool: "source-map",
-	devServer: {
-		// static: "./dist",
-		static: {
-			directory: path.resolve(__dirname, "./src/assets"),
-			publicPath: "/assets",
-		},
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-			inject: "body",
-			scriptLoading: "defer",
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-				generator: {
-					filename: "assets/[hash][ext][query]",
-				}, // To use the image file in html files
-			},
-		],
-	},
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    devtool: "eval-source-map", // https://webpack.js.org/configuration/devtool/
+    devServer: {
+        watchFiles: ["./src/index.html"], // By default webpack-dev-server only watches js files
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader", // This handles images in the html files
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
 };
